41d7a825b8ac2ca99b3b6fa103e899e452e73397
Tobias Schürg <tobiasschuerg@gmail.com>
2025-02-01 01:33:43 +0100

    configure github action to deplyo to pages
    Update vite.config.ts
    
    Update index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/keditrechner/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
